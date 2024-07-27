c4d0281e68f01e8c95ae317cd3f25168cce9c074
Marine M <marine.michelot-cic@ibm.com>
2024-07-27 10:56:31 +0200

    feat(model): add article model

---
+++ NewsApp/NewsApp/Models/Article.swift
@@ -0,0 +1,18 @@
+//
+//  Article.swift
+//  NewsApp
+//
+//  Created by Marine Michelot on 27/07/2024.
+//
+
+import Foundation
+
+struct Article {
+    let source: String
+    let author: String
+    let title: String
+    let subtitle: String
+    let publishedDate: String
+    let imageURL: String
+    let articleURL: String
+}
