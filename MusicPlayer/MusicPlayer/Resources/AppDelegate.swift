073b93b71a38f1060bde3e134b25e5a5eafa97dc
Marine M <marine.michelot-cic@ibm.com>
2024-08-29 22:26:34 +0200

    feat(Auth) : setup auth manager and entry point of the app

--- MusicPlayer/MusicPlayer/Resources/AppDelegate.swift
+++ MusicPlayer/MusicPlayer/Resources/AppDelegate.swift
@@ -16,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
        
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = TabBarViewController()
+        
+        window.rootViewController = AuthManager.shared.isSignedIn ? TabBarViewController() : UINavigationController(rootViewController: WelcomeViewController())
+        
         window.makeKeyAndVisible()
         self.window = window
         
