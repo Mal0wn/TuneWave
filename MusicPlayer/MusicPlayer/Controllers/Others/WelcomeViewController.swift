9b946a2f4dbbf8c1fbf123f62cec486380184308
Marine M <marine.michelot-cic@ibm.com>
2024-08-29 22:26:00 +0200

    feat(welcomescreen) : setup background color and title for welcome screen

--- MusicPlayer/MusicPlayer/Controllers/Others/WelcomeViewController.swift
+++ MusicPlayer/MusicPlayer/Controllers/Others/WelcomeViewController.swift
@@ -11,19 +11,19 @@ class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "TuneWave"
+        view.backgroundColor = .white
+        
+        // Create gradient
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.blueNonPhotoPastel.cgColor, UIColor.pinkPastel.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        
+        // add gradient like under layer of the view 
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
